8c7e6f89dd95119df7661d7f782e0bac7827bbce
Thorsten Otto <admin@tho-otto.de>
2018-07-13 05:28:17 +0200

    Fix prototypes, and kernel exec calls

--- gemma/src/gemma.h
+++ gemma/src/gemma.h
@@ -49,25 +49,6 @@
 #  define uchar unsigned char
 # endif
 
-struct xattr
-{
-	ushort	mode;
-	long	index;
-	ushort	dev;
-	ushort	rdev;		/* "real" device */
-	ushort	nlink;
-	ushort	uid;
-	ushort	gid;
-	long	size;
-	long	blksize;
-	long	nblocks;
-	ushort	mtime, mdate;
-	ushort	atime, adate;
-	ushort	ctime, cdate;
-	short	attr;
-	short	reserved2;
-	long	reserved3[2];
-};
 
 struct gemma_flags
 {
